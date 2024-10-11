25db9d38a5c4b88abb8515d19068ce48251f22ba
Ava Schmidt <avamschmidt07@gmail.com>
2024-10-11 08:05:54 -0500

    MVP and Stretch 3 Done +  Stretch 1 Started

---
+++ ClassmateAppSchmidt/CrazyCell.swift
@@ -0,0 +1,8 @@
+//
+//  CrazyCell.swift
+//  ClassmateAppSchmidt
+//
+//  Created by AVA SCHMIDT on 10/10/24.
+//
+
+import Foundation
---
+++ ClassmateAppSchmidt/ShowCrazyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShowCrazyViewController.swift
+//  ClassmateAppSchmidt
+//
+//  Created by AVA SCHMIDT on 10/10/24.
+//
+
+import UIKit
+
+class ShowCrazyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
